42c2429510a147c9f1850576bb619c9f52299b07
vferreir42 <33875000+vferreir42@users.noreply.github.com>
2018-05-29 18:35:01 +0200

    Update libft.h

--- includes/libft.h
+++ includes/libft.h
@@ -89,7 +89,7 @@ int					ft_strnequ(const char *s1, const char *s2, size_t n);
 char				*ft_strsub(char const *s, unsigned int start, size_t len);
 char				*ft_strjoin(char const *s1, char const *s2);
 char				*ft_strjoindel(char const *s1, char const *s2);
-char				*ft_strtrim(char const *s);
+char				*ft_strtrim(char *s);
 char				**ft_strsplit(char const *s, char c);
 char				*ft_itoa(long long n);
 void				ft_putchar(char c);
